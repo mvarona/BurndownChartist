fe179aa12e06a58eb8582b207c66b99b3c37a3c3
mvb <mvaronabueno@gmail.com>
2019-06-27 19:58:37 +0200

    typo fixed

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -24,7 +24,7 @@ export default class Footer extends React.Component {
                         </a>
 
                         <span className="pt-navbar-divider hide-if-small-750" />
-                        {v. 1.0}                 
+                        v. 1.0               
                     </div>
                 </nav>
             </div>
