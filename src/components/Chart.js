2968c59b8a18bc1fbdbfda3babb1f08f6073d1e8
mvb <mvaronabueno@gmail.com>
2021-02-19 17:19:56 +0100

    Test git

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -491,7 +491,7 @@ class Chart extends Component {
     componentWillMount() {
         this.setState({ loading: true });
         if (!this.props.user.token){
-            window.location.href = "https://burndownchartist.appspot.com/#login";
+            window.location.href = "http://localhost:3000/#login";
         }
         Todoist.getUser(this.props.user.token)
         .then(response => {
